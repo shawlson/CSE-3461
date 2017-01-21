8f7e486ed268d93c93869eba25610a2a90a7dde4
Dan Shawlson <danshawlson@gmail.com>
2017-01-21 17:40:09 -0500

    Get argument's filename for output file

--- Lab 1/copy.py
+++ Lab 1/copy.py
@@ -14,13 +14,14 @@ if (len(sys.argv) != 2):
     sys.exit('Usage: python3 copy.py <filename>')
 
 # Open file to be copied
-in_file = open(sys.argv[1], 'rb')
+in_file = open(sys.argv[-1], 'rb')
 
 # Create recv directory, if necessary
 os.makedirs('recv', exist_ok=True)
 
 # (Erase and) Create output file in new directory
-out_file = open('recv/' + sys.argv[1], 'wb')
+filename = os.path.basename(sys.argv[-1])
+out_file = open('recv/' + filename, 'wb')
 
 # Copy input file to output file
 copier.copy(in_file, out_file)
