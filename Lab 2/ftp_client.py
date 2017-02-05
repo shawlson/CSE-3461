9d6923a85b8f15543acd2fa51f9f0de5ed4de3c6
Dan Shawlson <danshawlson@gmail.com>
2017-02-04 22:23:15 -0500

    FTP client

--- Lab 2/ftp_client.py
+++ Lab 2/ftp_client.py
@@ -1,24 +1,64 @@
 # Dan Shawlson
 # CSE 3461, T/Th 12:45
-# Lab 1
+# Lab 2
 
-# Code to handle copying of bytes from input file to output file.
-# Trivial at this point, but I assume since the labs build off each
-# other this will become the logic to do file transfers over a network,
-# or something like that
+# FTP Client object establishes connection to server
+# and includes method to send file to server
 
+import socket
 import sys
 import os
 
-def copy(in_file, out_file):
-    
-    chunk_size = 512
+class FTPClient:
 
-    # Read 512 bytes from in_file and write them to out_file, until we
-    # reach the end of in_file
-    while True:
-        data_buffer = in_file.read(chunk_size)
-        if len(data_buffer) == 0:
-            break
-        else:
-            out_file.write(data_buffer)
+    @property
+    def address(self)
+        return self._address
+
+    @property
+    def port(self)
+        return self._port
+
+    def __init__(self, address, port)
+        self._address = address
+        self._port = port
+
+    def transfer(file, chunk_size=512):
+
+        # All future operations on the _socket object will fail
+        # once we close it, so it can't be an instance property
+        _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Connect to remote socket
+        _socket.connect((self._address, self._port))
+
+        # Set up protocol header
+        #   First 4 bytes - size of file in bytes, big endian
+        #   Next 20 bytes - name of file, assumed <= 20
+        chunk = bytearray()
+        file_size = os.path.getsize(file)
+        chunk += bytearray(file_size.to_bytes(4, 'big'))
+
+        padding = bytearray(20)
+        chunk += bytearray(file.name, 'ascii') + padding[len(file.name):]
+
+        if len(chunk != 24):
+            print('ERROR')
+            print(chunk)
+
+        # Start reading from file and sending chunks to server
+        chunk += file.read(chunk_size - 24)  # 24 bytes for protocol
+        _socket.send(chunk)
+
+        # Read and send until end of file
+        while True:
+            chunk = bytearray(file.read(chunk_size))
+            if len(chunk) == 0:
+                break
+            else:
+                _socket.send(chunk)
+
+        # Finished sending, shutdown and close socket
+        _socket.shutdown()
+        _socket.close()
+        
