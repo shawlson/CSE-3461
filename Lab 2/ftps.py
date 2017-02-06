05a47fe46715b145224efe27ade039e1fc5da557
Dan Shawlson <danshawlson@gmail.com>
2017-02-06 00:45:53 -0500

    Now i just wanna know if I can push to github

--- Lab 2/ftps.py
+++ Lab 2/ftps.py
@@ -3,10 +3,17 @@
 # Lab 2
 
 import socket
+import os
+import sys
 
+# Check usage and get port number
+if len(sys.argv) != 2:
+    sys.exit('usage: ftps.py <port number>')
+
+PORT = int(sys.argv[-1])
+
+# Listen on port specified until connection received
 HOST = ''
-PORT = 43210
-
 _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 _socket.bind((HOST, PORT))
 _socket.listen(1)
@@ -16,15 +23,16 @@ print('Received connection')
 data = conn.recv(512)
 # First 4 bytes indicate file size
 file_size = int.from_bytes(data[:4], byteorder='big')
-print(file_size)
 # Next 20 bytes indicate file name. Convert them to string
 # and strip any trailing null characters
 file_name = data[4:24].decode('ascii').rstrip('\0')
-print(file_name)
 
-# Create file and begin writing data to it
-out_file = open(file_name, 'wb')
+
+# Create file, and directory if necessary, and begin writing data to it
+os.makedirs('recv', exist_ok=True)
+out_file = open('recv/' + file_name, 'wb')
 out_file.write(data[24:])
+bytes_received = len(data[24:])
 
 # Receive the rest of the data in chunks and write them to file
 while True:
@@ -32,8 +40,15 @@ while True:
     if not data:
         break
     else:
+        bytes_received += len(data)
         out_file.write(data)
 
+# Check for mistakes in transmission
+if bytes_received != file_size:
+    print('ERROR')
+    print('{file_size} bytes expected, but {bytes_received} bytes received'\
+          .format(file_size, bytes_received))
+          
 # Close connection and file
 conn.close()
 out_file.close()
